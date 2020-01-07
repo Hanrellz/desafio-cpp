9a4b683ee62459f0d94daf03b5959fe468d40c17
Ulysses Fonseca <ulysses-2010@hotmail.com>
2020-01-07 15:59:11 -0300

    update readme

--- client/build_client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 ./src/client.cpp -o ./build/client -L/usr/local/lib -lzmq -lPocoJSON -lPocoFoundation
--- server/build_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 ./src/server.cpp -o ./build/server -L/usr/local/lib -lzmq -lPocoJSON -lPocoFoundation
