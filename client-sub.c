7a6bbc8dc4dd3eaa60b25d89cfa035ec3e3dd95b
Marco Ludwig <marcoludd@gmail.com>
2020-01-09 16:04:14 -0300

    modelo basico

--- client-sub.c
+++ client-sub.c
@@ -1,26 +1,30 @@
-//  Pubsub envelope subscriber
-
 #include "zhelpers.h"
 
-int main (void)
+int main(void)
 {
-    //  Prepare our context and subscriber
-    void *context = zmq_ctx_new ();
-    void *subscriber = zmq_socket (context, ZMQ_SUB);
-    zmq_connect (subscriber, "tcp://localhost:5563");
-    zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "VEICULO", 1);
+  /* cria um novo contexto **/
+  void *context = zmq_ctx_new();
+  /* cria um novo socket, para receber as mensagens **/
+  void *subscriber = zmq_socket(context, ZMQ_SUB);
+  /* conecta na porta específica para ficar monitorando
+    * @TODO valor da porta variável
+    **/
+  zmq_connect(subscriber, "tcp://localhost:5563");
+  /* inscreve no tópico VEICULO **/
+  zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "VEICULO", 1);
 
-    while (1) {
-        //  Read envelope with address
-        char *address = s_recv (subscriber);
-        //  Read message contents
-        char *contents = s_recv (subscriber);
-        printf ("[%s] %s\n", address, contents);
-        free (address);
-        free (contents);
-    }
-    //  We never get here, but clean up anyhow
-    zmq_close (subscriber);
-    zmq_ctx_destroy (context);
-    return 0;
+  while (1)
+  {
+    /* recebe a mensagem **/
+    char *address = s_recv(subscriber);
+    /* recebe o conteúdo e imprime eles na tela **/
+    char *contents = s_recv(subscriber);
+    printf("[%s] %s\n", address, contents);
+    /* libera a memória utilizada para não dar overflow**/
+    free(address);
+    free(contents);
+  }
+  zmq_close(subscriber);
+  zmq_ctx_destroy(context);
+  return 0;
 }
--- server-pub.c
+++ server-pub.c
@@ -1,31 +1,48 @@
 #include "zhelpers.h"
 #include <unistd.h>
+#include <stdlib.h>
 
-int main (void)
+int main(void)
 {
-    /* creates new context**/
-    void *context = zmq_ctx_new ();
-    /* creates new socket, to publish messages**/
-    void *publisher = zmq_socket (context, ZMQ_PUB);
-    /* bind specific port**/
-    zmq_bind (publisher, "tcp://*:5563");
-    char topico[] = "VEICULO";
+  /* cria um novo contexto **/
+  void *context = zmq_ctx_new();
 
-    /* keeps sending messages forever **/
-    while (1) {
-        /* Write a message with 3 parameters **/
-        s_sendmore (publisher, topico);
-        s_send (publisher, "parte_1");
-        s_sendmore (publisher, topico);
-        s_send (publisher, "parte_2");
-        s_sendmore (publisher, topico);
-        s_send (publisher, "timestamp");
-      
-        sleep (1);
-    }
-    //  We never get here, but clean up anyhow
-    zmq_close (publisher);
-    zmq_ctx_destroy (context);
-    return 0;
+  /* cria um novo socket, para enviar as mensagens **/
+  void *publisher = zmq_socket(context, ZMQ_PUB);
+
+  /* coloca a porta específica no socket criado **/
+  zmq_bind(publisher, "tcp://*:5563");
+  
+  /* definição do tópico, id e valor aleatório para velocidade **/
+  char topico[] = "VEICULO";
+  unsigned long int id = 0;
+  int velocidade = rand() % 255;
+
+  /* fica mandando mensagens indefinidamente **/
+  while (1)
+  {
+    /* altera o valor de id**/
+    ++id;
+    int length = snprintf(NULL, 0, "%ld", id);
+    char *str_id = malloc(length + 1);
+    snprintf(str_id, length + 1, "%ld", id);
+
+    /* escreve a mensagem com 3 parâmetros **/
+    s_sendmore(publisher, topico);
+    s_send(publisher, topico);
+    s_sendmore(publisher, topico);
+    s_send(publisher, str_id);
+
+    /* @TODO criar json com os valores da mensagem**/
+    s_sendmore(publisher, topico);
+    s_send(publisher, "mensagem - id, velocidade, timestamp");
+
+    /* libera memória **/
+    free(str_id);
+    /* fica mais fácil de acompanhar **/
+    sleep(1);
+  }
+  zmq_close(publisher);
+  zmq_ctx_destroy(context);
+  return 0;
 }
-
